3b81348ac14a2c4d8c1f7a302906b490556fbc2f
Nico538e <nicolai-strand@hotmail.com>
2025-12-17 11:05:51 +0100

    Refactor code structure and remove redundant code blocks

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { useState } from 'react'
-import { NavLink } from "react-router";
+import { NavLink, Link } from "react-router";
 import styles from "./Header.module.css";
 import Login from "../login/Login";
+import '../../App.css';
 
 const Welcome= ({username, logout}) => (
       <div className={styles.Header}>
@@ -23,6 +24,13 @@ const Header = ({ headers }) => {
 
   return (
     <div className={styles.header}>
+
+      <div>
+        <Link to="/home" className={styles.headerLogo} >
+          <img src="/logo.png" alt="Logo" className={styles.headerLogo}/>
+        </Link>
+      </div>
+
         <nav>
         {headers.map((header, index) => (
           <NavLink
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,33 @@
+import styles from './Home.module.css';
+
+
 function Home(){
     return(
-        <>
-        <div>
-          <h1>hej Home</h1>
+        
+        <div className={styles.homeContainer}>
+
+          <section className={styles.heroSection}>
+
+            <div className={styles.heroContent}>
+              <h1 className={styles.heroTitle}>Velkommen til en verden af musik </h1>
+            <p className={styles.heroText}>
+            Dyk ned i en ubegrænset samling af rytmer og melodier. Uanset om du er til de nyeste hits, 
+            tidløse klassikere eller undergrundens skjulte perler, har vi lyden til dit øjeblik.
+            <br/>
+            <br />
+            Tjek det ud i toppen af siden!
+            </p>
+          
+            </div> 
+
+          <div className={styles.imageContainer}>
+            <img src="/homepage.png" alt="homepage" className={styles.imgimg} />
+          </div>
+
+          </section>
         </div>
-        </>
-    )
+        
+    );
 
 }
 export default Home;

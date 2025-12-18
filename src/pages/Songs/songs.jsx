5dca05d20588f93a9de56586770f69cad3bfebb8
niki03125 <Nikita.po@hotmail.com>
2025-12-18 11:51:32 +0100

    Fix Songs component: Correct fetchSongs invocation, update key prop, and adjust album display

--- src/pages/Songs/songs.jsx
+++ src/pages/Songs/songs.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import styles from "./songs.module.css"
 function Songs(){
     const [songs, setSongs] = useState([]);
 
@@ -9,13 +8,13 @@ function Songs(){
             const data = await response.json();
             setSongs(data);
         }
-        fetchSongs;  
+        fetchSongs();  
     }, []);  
 
     return(
         <>
         <div>
-            <h1 className={styles.title}>Songs</h1>
+            <h1>Songs</h1>
 
             <table>
                 <thead>
@@ -29,10 +28,10 @@ function Songs(){
                 </thead>
                 <tbody>
                     {songs.map((song) =>(
-                        <tr key = {songs.id}>
+                        <tr key = {song.songId}>
                             <td>{song.title}</td>
                             <td>{song.artistName}</td>
-                            <td>{song.albumId}</td>
+                            <td>{song.album}</td>
                             <td>{song.duration}</td>
                             <td>{song.releaseDate.substring(0, 4)}</td>
                         </tr>
